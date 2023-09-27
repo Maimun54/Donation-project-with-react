fe8473e6bd6f097c77ae9b36f5414a9617518ef2
Maimun54 <maimunislam@gmail.com>
2023-09-27 21:06:26 +0600

    Website bug fixing

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -7,8 +7,9 @@ const Donation = () => {
     const [donation,setDonation]=useState([]) ||"";
      const [noDonatin,setNodonation]=useState(false)
      const [donationLength,setDonationLength]=useState(4)
+
     useEffect(()=>{
-    const donationItem =JSON.parse(localStorage.getItem('Donation') )
+const donationItem =JSON.parse(localStorage.getItem('Donation')) || []
     if(donation){
         setDonation(donationItem)
     }
@@ -24,7 +25,7 @@ const Donation = () => {
             {noDonatin?<p>{noDonatin}</p>:<div>
                 
    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-5 px-10">
-     {donation.slice(0,donationLength).map(card=><Card key={card.id} card={card}></Card>)}
+     {donation?.slice(0,donationLength)?.map(card=><Card key={card.id} card={card}></Card>)}
      <div  className={donationLength===donation.length ? 'hidden':''}>
         <button onClick={()=>setDonationLength(donation.length)}  className="btn btn-primary">Show All</button>
      </div>
--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,6 +1,8 @@
 
 
 const Statistics = () => {
+const donationItem =JSON.parse(localStorage.getItem('Donation')) || {}
+    console.log(donationItem)
     return (
         <div>
             Statistics
